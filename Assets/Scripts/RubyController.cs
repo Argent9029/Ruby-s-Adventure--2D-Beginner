f89bb70f482afd91dfdd54ad3e747cb04b4618c8
Argent <136908067+Argent9029@users.noreply.github.com>
2024-04-04 16:56:40 +0100

    Tutorial

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+
+    [SerializeField] float speed = 0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //FPS Controller
+        /*
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60; 
+        */
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement Inputs Horizontal/Vertical
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //Calculate movement
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
